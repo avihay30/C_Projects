19d1b29eaaccbc456660c465839bc9c601142dc2
Avihay-Laptop <avihay30@gmail.com>
2021-04-18 20:43:40 +0300

    fixed hw3

--- HW/Hw1/Hw1/Ex_3.c
+++ HW/Hw1/Hw1/Ex_3.c
@@ -1,34 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
-void incrementHistogram(int[], int, char);
+void createHistogram(char* str);
 void printHistogram(int[], int);
 
-#define N 4
-#define TRUE 1
-#define FALSE 0
+#define N 6
+#define STR_SIZE 40
 
 int main()
 {
-	char *str;
-/*	str = (char*)(mall)
-	int cntArray[N], numOfChars = 0;
+	char str[STR_SIZE + 1], chr;
+	int i;
 	printf("Enter your string: ");
-	do {
-		scanf("%c", &chr);
-		numOfChars++;
+	for (i = 0; i < STR_SIZE && str[i - 1] != '!'; i++) {
+		scanf("%c", &str[i]);
+	}
+	str[i] = '\0'; // ending string with '\0'
+	if (str[strlen(str) - 1] != '!') {
+		printf("InputError: Histogram could not be created. invalid ending of string, should be ended with '!'\n");
+		return 1;
+	}
 
-		
-	} while (chr != "!");
-	*/
+	createHistogram(str);
 	return 0;
 }
 
-void incrementHistogram(int arr[], int size, char chr)
+void createHistogram(char *str)
 {
-	int indexInAZ = chr - 'a';
-	if (indexInAZ <= size)
-		arr[indexInAZ]++;
+	int cntArray[N] = { 0 }, indexInAZ;
+	for (; *str != '\0' && *str != '!'; str++) {
+		indexInAZ = *str - 'a';
+		if (indexInAZ >= 0 && indexInAZ < N)
+			cntArray[indexInAZ]++;
+	}
+	printHistogram(cntArray, N);
 }
 
 void printHistogram(int arr[], int size)
