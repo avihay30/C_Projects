49c87a9748380a4dc3909362581d4bae748643a2
Avihay-Laptop <avihay30@gmail.com>
2022-03-23 10:57:52 +0200

    added .sh file

---
+++ OperationSystems/HW/HW1/HW1/Advanced_shell.sh
@@ -0,0 +1,8 @@
+cc Advanced_shell.c -o Advanced_shell
+cc CreateMenu.c -o CreateMenu
+cc getMenu.c -o getMenu
+cc getPrice.c -o getPrice
+cc MakeOrder.c -o MakeOrder
+cc getOrderNum.c -o getOrderNum
+
+./Advanced_shell
